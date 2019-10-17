c5a69c26c651721aa2d36695a3a9acaf659d1a1a
andythebreaker <43373581+andythebreaker@users.noreply.github.com>
2019-10-17 20:39:09 +0800

    Update doall.sh

--- doall.sh
+++ doall.sh
@@ -3,6 +3,12 @@ printf "name:(without .s)"
 read Name
 s=${Name}".s"
 o=${Name}".o"
+i=${Name}".image"
+c=${Name}".conv"
+t=${Name}".txt"
 sde-as ${s} -g -o ${o}
-
-
+sde-ld ${o} -Ttext 0x0000800 -o ${i}
+sde-conv ${i} -o ${c}
+sde-objdump ${i} -D > ${t}
+printf "You have finished converting..."
+ls
