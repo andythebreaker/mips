4706e74be9ac66aa2193e44af66ec88c0da55cb2
andythebreaker <43373581+andythebreaker@users.noreply.github.com>
2019-10-17 19:28:13 +0800

    Add files via upload

---
+++ practice_2/data_struct.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+struct student{
+    
+    int mathGrade;
+    int csGrade;
+    int englishGrade;
+} ;
+
+main(){
+
+volatile struct student* A =(struct student*) 0x20000000;
+volatile struct student* B =(struct student*) 0x20000020;
+struct student s1 = {60,70,70};  
+struct student s2 = {70,50,80};  
+
+ *A = s1;
+ *B = s2;  
+}
